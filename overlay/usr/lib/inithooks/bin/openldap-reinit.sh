ad64dd8dc17b5ea5d91566494e2f73ceed551727
Alon Swartz <alon@turnkeylinux.org>
2012-07-19 11:53:34 +0300

    create inithook to reinitialize ldap with domain and admin password
    - checks if slapd is already running
    - preseeds slapd and initiates dpkg-reconfigure
    - creates Users/Groups OU's and default posixGroup (convenience)
    - updates phpldapadmin with new basedn
    - updates ldapscripts with new basedn and sets password
    - restarts slapd if it was running, else it is stopped

---
+++ overlay/usr/lib/inithooks/bin/openldap-reinit.sh
@@ -0,0 +1,86 @@
+#!/bin/bash -e
+
+fatal() {
+    echo "fatal: $@" 1>&2
+    exit 1
+}
+
+usage() {
+cat<<EOF
+Syntax: $(basename $0) domain password
+Re-initialize LDAP
+
+Arguments:
+    domain          # LDAP domain
+    password        # LDAP admin password
+
+EOF
+    exit 1
+}
+
+if [[ "$#" != "2" ]]; then
+    usage
+fi
+
+LDAP_DOMAIN=$1
+LDAP_PASS=$2
+LDAP_BASEDN="dc=`echo $LDAP_DOMAIN | sed 's/^\.//; s/\./,dc=/g'`"
+
+SLAPD_RUNNING=$(/etc/init.d/slapd status > /dev/null; echo $?)
+
+# re-configure ldap
+debconf-set-selections << EOF
+slapd slapd/backend string HDB
+slapd slapd/allow_ldap_v2 boolean false
+slapd slapd/no_configuration boolean false
+slapd slapd/move_old_database boolean true
+slapd slapd/purge_database boolean true
+slapd slapd/password1 password $LDAP_PASS
+slapd slapd/password2 password $LDAP_PASS
+slapd slapd/domain string $LDAP_DOMAIN
+slapd shared/organization string $LDAP_DOMAIN
+EOF
+
+rm -rf /var/backups/slapd-
+rm -rf /var/backups/*.ldapdb
+DEBIAN_FRONTEND=noninteractive dpkg-reconfigure slapd
+
+# create Users/Groups OU's and default posixGroup
+ldapadd -x -D cn=admin,$LDAP_BASEDN -w $LDAP_PASS <<EOL
+dn: ou=Groups,$LDAP_BASEDN
+objectclass: organizationalUnit
+objectclass: top
+ou: Groups
+
+dn: cn=default,ou=Groups,$LDAP_BASEDN
+cn: default
+gidnumber: 500
+objectclass: posixGroup
+objectclass: top
+
+dn: ou=Users,$LDAP_BASEDN
+objectclass: organizationalUnit
+objectclass: top
+ou: Users
+EOL
+
+# update phpldapadmin
+CONF=/var/www/phpldapadmin/config/config.php
+sed -i "s|bind_id.*|bind_id','cn=admin,$LDAP_BASEDN');|" $CONF
+
+# update ldapscripts
+CONF=/etc/ldapscripts/ldapscripts.conf
+sed -i "s|^BINDDN.*|BINDDN=\"cn=admin,$LDAP_BASEDN\"|" $CONF
+sed -i "s|^SUFFIX.*|SUFFIX=\"$LDAP_BASEDN\"|" $CONF
+
+CONF=/etc/ldapscripts/ldapscripts.passwd
+echo -n $LDAP_PASS > $CONF
+chmod 640 $CONF
+
+# restart slapd if it was running, or stop it
+if [ "$SLAPD_RUNNING" == "0" ]; then
+    /etc/init.d/slapd restart
+else
+    /etc/init.d/slapd stop
+fi
+
---
+++ overlay/usr/lib/inithooks/bin/openldap.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+"""Set OpenLDAP domain and admin password (will reinitialize LDAP)
+
+Option:
+    --pass=     unless provided, will ask interactively
+    --domain=   unless provided, will ask interactively
+                DEFAULT=example.com
+
+"""
+
+import os
+import sys
+import getopt
+
+from dialog_wrapper import Dialog
+from executil import system
+
+def usage(s=None):
+    if s:
+        print >> sys.stderr, "Error:", s
+    print >> sys.stderr, "Syntax: %s [options]" % sys.argv[0]
+    print >> sys.stderr, __doc__
+    sys.exit(1)
+
+DEFAULT_DOMAIN="example.com"
+
+def main():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
+                                       ['help', 'pass=', 'domain='])
+    except getopt.GetoptError, e:
+        usage(e)
+
+    domain = ""
+    password = ""
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
+            usage()
+        elif opt == '--pass':
+            password = val
+        elif opt == '--domain':
+            domain = val
+
+    if not password:
+        d = Dialog('TurnKey Linux - First boot configuration')
+        password = d.get_password(
+            "OpenLDAP Password",
+            "Enter new password for the OpenLDAP 'admin' account.")
+
+    if not domain:
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+        domain = d.get_input(
+            "OpenLDAP Domain",
+            "Enter the OpenLDAP domain.",
+            DEFAULT_DOMAIN)
+
+    if domain == "DEFAULT":
+        domain = DEFAULT_DOMAIN
+
+    script = os.path.join(os.path.dirname(__file__), 'openldap-reinit.sh')
+    system(script, domain, password)
+
+if __name__ == "__main__":
+    main()
+
